147455950638db01f7b78ca0c3f9104a7578d705
Angel Luis Robles Fernandez <a.l.robles.fernandez@gmail.com>
2023-07-07 07:25:46 -0700

    changing get_log_reg_coefficients.R

--- R/get_log_reg_coefficients.R
+++ R/get_log_reg_coefficients.R
@@ -27,7 +27,7 @@ get_log_reg_coefficients <- function(incidence, phydist){
   df <- do.call(rbind, lapply(names(focal), function(x) data.frame(colnames(incidence),
                                                                    focal[[x]], phydist[ , focal[[x]] ], as.numeric(incidence[x, ]), x) ) )
   colnames(df) <- c("tohost", "fromhost", "phydist", "suscept", "incidence")
-
+  df <- subset(df, tohost != fromhost)
   log_out <- stats::glm(df$suscept ~ df$phydist, family = stats::binomial(link="logit") )
   stats_log_out <- stats::coef(summary(log_out) )
   conf_int <- stats::confint.default(log_out)
